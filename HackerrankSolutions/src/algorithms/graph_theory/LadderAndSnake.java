b0512ce560a4f6896f57bd52336278fe53056732
z080328 <zhouqiao_ntu@yahoo.com>
2016-09-17 17:13:57 -0700

    added comments

--- HackerrankSolutions/src/algorithms/graph_theory/LadderAndSnake.java
+++ HackerrankSolutions/src/algorithms/graph_theory/LadderAndSnake.java
@@ -103,6 +103,18 @@ class LadderAndSnakeGraph{
     }
     
     public int bfSearch(int start_ix){
+    /*
+    Breadth-first search for unweighted shortest path: basic idea
+
+    By distance between two nodes u,v we mean the number of edges on the shortest path between u and v . Now:
+    Start at the start vertex s . It is at distance 0 from itself, and there are no other nodes at distance 0
+    Consider all the nodes adjacent to s . These all are at distance at most 1 from s (maybe less than 1, if s
+    has an edge to itself; but then we would have found a shorter path already) and there are no other nodes at distance 1
+    Consider all the nodes adjacent to the nodes adjacent to s . These are all at distance at most 2 from s (maybe less 
+    than 2; but then we would have found a shorter path already) and there are no other nodes at distance 2
+    ... and so on. In this breadth-first search, as soon as we visit a node in the graph, we know the shortest path from s to it; 
+    and so by the time we have visited all the nodes in the graph, we know the shortest path from s to each of them        
+    */
         //add te current node into the queue and mark as visited
         queue.add(start_ix);
         distance[start_ix] = 0;
