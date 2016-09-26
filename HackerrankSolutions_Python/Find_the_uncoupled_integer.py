8b88b6e1afa2ecab697072c4cc3ac5783d10d64e
z080328 <zhouqiao_ntu@yahoo.com>
2016-09-25 21:44:25 -0700

    code solution for the uncoupled integer problem in hackerrank.
    https://www.hackerrank.com/contests/programming-interview-questions/challenges/find-uncoupled-integer

---
+++ HackerrankSolutions_Python/Find_the_uncoupled_integer.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 25 12:22:02 2016
+
+Find the uncoupled integer
+
+@author: Zhou Qiao
+"""
+
+
+# wait for user inputs
+my_string = input()
+# get number of numbers to be processed
+num_numbers = len(my_string)
+# use a dict object to store the key value pairs, 
+# where key is the number and val is the number of occurence 
+my_dict = dict()
+
+# for each number
+for i in range(num_numbers):
+    number = my_string[i]
+    # if it is in the dict, increment the count by 1
+    if my_dict.has_key(number):
+        my_dict[number] += 1
+    else:#if it is not in the dict, add to dict and initialize the count to 1
+        my_dict[number] = 1
+
+#get the list of keys and values as two lists 
+key_set = my_dict.keys()
+val_set = my_dict.values()
+#visit each key value pairs in the dict and output the Key whose value is 
+#odd number (i.e., not divisorble by 2)
+for i in range(len(key_set)):
+    key = key_set[i]
+    val = val_set[i]
+    if val % 2 == 1:
+        print key
+        break;
